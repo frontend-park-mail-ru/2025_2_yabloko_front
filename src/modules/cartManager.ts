af66916e455e47bea24b6552c8464f86debfeef6
antiqueMouse <s.yakovlev06@yahoo.com>
2025-11-07 22:50:05 +0300

    check user auth changed

--- src/modules/cartManager.ts
+++ src/modules/cartManager.ts
@@ -5,7 +5,7 @@ const CART_KEY = 'guest_cart'
 
 export async function getCartFromStorage(): Promise<CartItem[]> {
 	try {
-		if (authManager.checkAuth()) {
+		if (await authManager.isAuthenticated()) {
 			const cart = await StoreApi.getUserCart()
 			return cart.items as CartItem[]
 		} else {
@@ -21,7 +21,6 @@ export async function getCartFromStorage(): Promise<CartItem[]> {
 export async function saveCartToStorage(items: CartItem[]): Promise<void> {
 	try {
 		if (await authManager.isAuthenticated()) {
-			console.log("hui");
 			const updateItems = items.map(item => ({
 				id: item.id,
 				store_id: "c45a7b64-df32-4e84-b2cb-85a3b8e6b0fc",
