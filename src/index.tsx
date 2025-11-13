ce5a907c39aed8502be19379f14adea2b7bf4dc4
antiqueMouse <s.yakovlev06@yahoo.com>
2025-11-13 04:24:18 +0300

    sw fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,18 @@ import { authManager } from './modules/authManager'
 import { initRouter } from './modules/router'
 ;(window as any).h = h
 
-// if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-// 	window.addEventListener('load', () => {
-// 		navigator.serviceWorker
-// 			.register('/sw.js', { scope: '/' })
-// 			.then(registration => {
-// 				console.log('Service Worker registered with scope:', registration.scope)
-// 			})
-// 			.catch(error => {
-// 				console.error('Service Worker registration failed:', error)
-// 			})
-// 	})
-// }
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('/sw.js', { scope: '/' })
+			.then(registration => {
+				console.log('Service Worker registered with scope:', registration.scope)
+			})
+			.catch(error => {
+				console.error('Service Worker registration failed:', error)
+			})
+	})
+}
 
 if (typeof document !== 'undefined') {
 	await authManager.checkAuth()
