23be4b3ac6300342710906e1ef85d6536066658a
antiqueMouse <s.yakovlev06@yahoo.com>
2025-11-15 14:57:47 +0300

    iframe fix 29

--- support/webpack.config.js
+++ support/webpack.config.js
@@ -1,71 +1,75 @@
-const path = require("path");
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const webpackModule = {
-  rules: [
-    {
-      test: /\.tsx?$/,
-      use: ["babel-loader"],
-      exclude: /node_modules/,
-    },
-    {
-      test: /\.module\.scss$/,
-      use: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: {
-              mode: "local",
-              localIdentName: "[name]__[local]--[hash:base64:5]",
-              exportLocalsConvention: "dashesOnly",
-            },
-          },
-        },
-        "sass-loader",
-      ],
-    },
-    {
-      test: /\.module\.css$/,
-      use: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: {
-              mode: "local",
-              auto: true,
-              exportGlobals: true,
-              localIdentName: "[name]__[local]--[hash:base64:5]",
-            },
-          },
-        },
-      ],
-    },
-    {
-      test: /\.css$/,
-      exclude: /\.module\.css$/,
-      use: ["style-loader", "css-loader"],
-    },
-  ],
-};
+	rules: [
+		{
+			test: /\.tsx?$/,
+			use: ['babel-loader'],
+			exclude: /node_modules/,
+		},
+		{
+			test: /\.module\.scss$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						modules: {
+							mode: 'local',
+							localIdentName: '[name]__[local]--[hash:base64:5]',
+							exportLocalsConvention: 'dashesOnly',
+						},
+					},
+				},
+				'sass-loader',
+			],
+		},
+		{
+			test: /\.module\.css$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						modules: {
+							mode: 'local',
+							auto: true,
+							exportGlobals: true,
+							localIdentName: '[name]__[local]--[hash:base64:5]',
+						},
+					},
+				},
+			],
+		},
+		{
+			test: /\.css$/,
+			exclude: /\.module\.css$/,
+			use: ['style-loader', 'css-loader'],
+		},
+	],
+}
 
 const resolveScripts = {
-  extensions: [".tsx", ".ts", ".js", ".css", ".scss"],
-};
-const distPath = path.resolve(__dirname, 'dist')
-const mode = process.env.PRODUCTION == "true" ? "production" : "development";
+	extensions: ['.tsx', '.ts', '.js', '.css', '.scss'],
+}
 
-module.exports = [
-  {
-    mode,
-    devtool: "source-map",
-    entry: "./src/index.tsx",
-    module: webpackModule,
-    resolve: resolveScripts,
-    output: {
-      filename: "bundleSupport.js",
-      path: distPath,
-    },
-  },
-];
+const distPath = path.resolve(__dirname, 'dist')
+const mode = process.env.PRODUCTION == 'true' ? 'production' : 'development'
+
+export default [
+	{
+		mode,
+		devtool: 'source-map',
+		entry: './src/index.ts',
+		module: webpackModule,
+		resolve: resolveScripts,
+		output: {
+			filename: 'bundleSupport.js',
+			path: distPath,
+		},
+	},
+]
