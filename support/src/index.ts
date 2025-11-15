4cf15262b935633b90614c379b7faf04084566ab
antiqueMouse <s.yakovlev06@yahoo.com>
2025-11-15 14:59:37 +0300

    iframe fix 30

--- support/src/index.ts
+++ support/src/index.ts
@@ -1,9 +1,9 @@
 import { h } from "@antiquemouse/framework";
-import { SupportWidget } from "./components/SupportWidget";
+import { SupportApp } from './components/SupportWidget'
 
 (window as any).h = h;
 
 if (typeof document !== "undefined") {
-  const widget = new SupportWidget();
+  const widget = new SupportApp()
   widget.mount(document.body);
 }
