8a4da8f96dd39eb9c5c64bef1f54345e62c6df26
antiqueMouse <s.yakovlev06@yahoo.com>
2025-12-05 02:28:19 +0300

    cart fix 3

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -77,7 +77,6 @@ export const Cart = defineComponent({
 				on={{
 					click: (e: Event) => {
 						if (e.target === e.currentTarget) {
-							e.stopPropagation()
 							props.onClose()
 						}
 					},
