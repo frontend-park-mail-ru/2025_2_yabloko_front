41916c0689e388ee28eb2c8ec7fb461e71ff3aac
antiqueMouse <s.yakovlev06@yahoo.com>
2025-12-11 11:01:28 +0300

    history fix 38

--- src/components/Batch/Batch.tsx
+++ src/components/Batch/Batch.tsx
@@ -52,7 +52,11 @@ export const Batch = defineComponent({
 						<Card
 							key={store.id}
 							store={store}
-							onCardClick={this.props.onCardClick() && (() => this.props.onCardClick.store.id)}
+							onCardClick={
+								this.props.onCardClick
+									? () => this.props.onCardClick(store.id)
+									: null
+							}
 						/>
 					))}
 				</div>
