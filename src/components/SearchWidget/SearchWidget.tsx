476cc816e0ac2d4cf8f5435502c5891b2f0bed2b
antiqueMouse <s.yakovlev06@yahoo.com>
2025-12-11 19:33:42 +0300

    adding search 6

--- src/components/SearchWidget/SearchWidget.tsx
+++ src/components/SearchWidget/SearchWidget.tsx
@@ -3,7 +3,7 @@ import { navigate } from '../../modules/router'
 import { StoreApi } from '../../modules/storeApi'
 import { Card } from '../Card/Card'
 import { ProductCard } from '../ProductCard/ProductCard'
-import styles from './SearchModal.module.scss'
+import styles from './SearchWidget.module.scss'
 
 interface SearchModalProps {
 	onClose: () => void
