de6284096db7f944f09a22e7fe9590c923a04a05
antiqueMouse <s.yakovlev06@yahoo.com>
2025-12-11 10:45:02 +0300

    history fix 35

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -82,7 +82,7 @@ export const MainPage = defineComponent({
 					/>
 
 					<Batch
-						//key={`${this.state.currentFilter.type}-${this.state.currentFilter.id}`}
+						key={`${this.state.currentFilter.type}-${this.state.currentFilter.id}`}
 						filterType={this.state.currentFilter.type}
 						filterId={this.state.currentFilter.id}
 						onCardClick={storeId => navigate(`/store/${storeId}`)}
